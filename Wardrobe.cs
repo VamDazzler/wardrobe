2d7542246421bbdaa8c555b50f31367bd6616698
VamDazzler <withheld@nowhere.net>
2019-02-17 23:08:52 -0500

    Updated version number

--- Wardrobe.cs
+++ Wardrobe.cs
@@ -46,7 +46,7 @@ namespace chokaphi_VamDazz
             try
             {
                 disableUpdate = true;
-                pluginLabelJSON.val = "Wardrobe v1.0 (by VamDazzler)";
+                pluginLabelJSON.val = "Wardrobe v1.1.0 (by VamDazzler)";
 
                 // Obtain our person
                 myPerson = containingAtom;
