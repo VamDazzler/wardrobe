e58c0095f5156f455889ae24c22d478c2b1c3e29
VamDazzler <withheld@nowhere.net>
2019-02-24 13:22:55 -0500

    Version Bump

--- src/Wardrobe.cs
+++ src/Wardrobe.cs
@@ -37,7 +37,7 @@ namespace VamDazzler
             try
             {
                 disableUpdate = true;
-                pluginLabelJSON.val = "Wardrobe v2.0.0 (by VamDazzler)";
+                pluginLabelJSON.val = "Wardrobe v2.0.1 (by VamDazzler)";
 
                 // Obtain our person
                 if( containingAtom == null )
